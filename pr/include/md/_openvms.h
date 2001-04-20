ce58cdce5760c8449757d7a59763b08ba02f1410
cvs2hg <cvs2hg@localhost>
2001-04-20 01:25:01 +0000

    fixup commit for branch 'MOZILLA_0_9_BRANCH'

--- pr/include/md/_openvms.h
+++ pr/include/md/_openvms.h
@@ -40,6 +40,7 @@
 #define _PR_VMBASE		0x30000000
 #define _PR_STACK_VMBASE	0x50000000
 #define _MD_DEFAULT_STACK_SIZE	131072L
+#define _MD_MINIMUM_STACK_SIZE	131072L
 
 /*
 ** This is not defined on OpenVMS. I believe its only used in GC code, and
@@ -80,6 +81,17 @@ struct ip_mreq {
 #define _PR_USE_POLL
 #define _PR_STAT_HAS_ONLY_ST_ATIME
 #define _PR_NO_LARGE_FILES
+
+/* IPv6 support */
+#define _PR_HAVE_GETIPNODEBYNAME
+#define _PR_HAVE_GETIPNODEBYADDR
+#define _PR_INET6_PROBE
+#ifndef _PR_INET6
+#define AF_INET6 26
+#define AI_V4MAPPED 0x00000010
+#define AI_ALL      0x00000008
+#define AI_ADDRCONFIG 0x00000020
+#endif
 
 #undef  USE_SETJMP
 
